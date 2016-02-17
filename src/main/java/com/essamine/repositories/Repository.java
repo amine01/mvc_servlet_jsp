e581cfa03b6b8d0a9a057849b77fe6ba9b046a89
@Amine <essadkaoui01@gmail.com>
2016-02-17 12:47:25 +0000

    fix problemes through deployment assembly + fix url to the database

--- src/main/java/com/essamine/repositories/Repository.java
+++ src/main/java/com/essamine/repositories/Repository.java
@@ -6,27 +6,28 @@ import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 
 public class Repository<E> {
-	private final EntityManager em = Persistence.createEntityManagerFactory("persondb").createEntityManager();
+
+	private final EntityManager em = Persistence.createEntityManagerFactory("unit").createEntityManager();
 	private final Class<E> entityClass;
 
 	public Repository(Class<E> entityClass) {
 		this.entityClass = entityClass;
 	}
 
-	public void save(E entity) {
-		em.merge(entity);
-	}
-
 	public E find(long id) {
 		return em.find(entityClass, id);
 	}
 
-	public void delete(E entity) {
-		em.remove(entity);
-	}
-
 	public List<E> findAll() {
 		return em.createQuery("from " + entityClass.getSimpleName(), entityClass).getResultList();
+	}
+
+	public void save(E entity) {
+		em.merge(entity);
+	}
+
+	public void delete(E entity) {
+		em.remove(entity);
 	}
 
 }
