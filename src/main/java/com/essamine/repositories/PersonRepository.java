19d434b6c83b7b056f88b96353bfc15910b800ae
@Amine <essadkaoui01@gmail.com>
2016-02-23 15:36:36 +0000

    refactoring the project spring hibenatefinal version NO ERRORs

--- src/main/java/com/essamine/repositories/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.essamine.repositories;
-
-import com.essamine.entities.Person;
-
-public class PersonRepository extends Repository<Person> {
-	public PersonRepository() {
-		super(Person.class);
-	}
-}
